7fdfdf6bff9749170b4da248cc67f637ff1542ce
w8 <@w8>
2017-11-26 19:14:29 -0200

    receber form por querySelector

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,7 +1,21 @@
 class NegociacaoController{
     
-    executa(event){
-        event.preventDefault();
-        console.log('C ok')
+    constructor(){
+        let $ = document.querySelector.bind(document);
+
+        this._data = $('#data');
+        this._quantidade = $('#quantidade');
+        this._valor = $('#valor');
+    }
+
+    adiciona(event){
+        event.preventDefault();  
+
+        new Negociacao(
+            new Date(this._data.value),
+            this._quantidade.value,
+            this._valor.value);
+
+            console.log(this._data.value)
     }
 }
